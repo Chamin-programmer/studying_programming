4dc06854844cd5b8e4630c0a57d0a900a56aca73
Chamin <65225845+Chamin-programmer@users.noreply.github.com>
2022-07-23 22:58:39 +0900

    未使用のimport文の削除

--- q4.java
+++ q4.java
@@ -4,8 +4,6 @@ import java.io.IOException;
 import java.io.PrintWriter;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Scanner;
-import java.util.Collections;
 
 public class q4 {
     public static void main(String[] args) {
